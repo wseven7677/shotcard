fe0b1e21518d93deb65fde0618bbf79deb69b573
localWseven <caoyu_92@126.com>
2017-08-20 00:57:52 +0800

    卡页的颜色样式

--- src/scripts/sub-scripts/CardBook.jsx
+++ src/scripts/sub-scripts/CardBook.jsx
@@ -6,10 +6,38 @@ class CardBook extends React.Component {
 	render() {
 
 		var localCard = localStorage.getItem('shotcard-card').split(','),
-			contents=[];
+			contents=[],
+			liStyle;
 
 		cardCollection.forEach(function(value,index){
-			contents.push(<li>{value.card}，{localCard[index]}</li>);
+			if(localCard[index] !== '-1'){
+				switch(value.to - value.from + 1){
+					case 80:
+						liStyle = {backgroundColor: 'rgba(0,0,0,.2)',color: 'rgba(0,0,0,.8)'};
+						break;
+					case 60:
+						liStyle = {backgroundColor: 'rgba(53,224,73,.2)',color: 'rgba(53,224,73,.8)'};
+						break;
+					case 40:
+						liStyle = {backgroundColor: 'rgba(41,155,241,.2)',color: 'rgba(41,155,241,.8)'};
+						break;
+					case 20:
+						liStyle = {backgroundColor: 'rgba(183,41,214,.2)',color: 'rgba(183,41,214,.8)'};
+						break;
+					case 10:
+						liStyle = {backgroundColor: 'rgba(240,154,19,.2)',color: 'rgba(240,154,19,.8)'};
+						break;
+					default:
+						console.log('抽到的句子范围有问题。');
+				}
+			}else{
+				liStyle = {};
+			}
+			if(index == 3){
+				liStyle = {backgroundColor: 'rgba(41,155,241,.2)',color: 'rgba(41,155,241,.8)'};
+			}
+			contents.push(<li style={liStyle}>{value.card}（{localCard[index]}）</li>);
+
 		});
 
 		return <div className='cardBook'>
